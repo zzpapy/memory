fbfc64fd383feaec17c73c28e9316dfd657c962d
zzpapy <gregory.pace@hotmail.fr>
2020-07-03 15:59:41 +0200

    first

---
+++ public/script.js
@@ -0,0 +1,6 @@
+console.log('toto')
+var click = document.getElementsByClassName("test");
+click.onclick = console.log("toto")
+// $(".toto").on("click",() =>{
+//     console.log("toto")
+// })
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './Card'
+import GuessCount from './GuessCount'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCardClick = this.handleCardClick.bind(this);
+  }
+  handleCardClick(card){
+    console.log(card, 'clicked')
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+      const won = new Date().getSeconds() % 2 === 0
+     console.log(won)
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNÉ !</p>}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
